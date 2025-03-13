40dadab79e0e7454bdae85d40f089cee1b998813
abtic <abthical@gmail.com>
2025-03-13 04:17:36 +0330

    Gray_code_Generator.py

--- Gray_code_Generator.py
+++ Gray_code_Generator.py
@@ -5,21 +5,18 @@ NON = 2** numberOfChars;
 gray = [""] * NON;
 
 def generate(n):
-
-
+    
     for t in range(0,int(n/2)):
-
         gray[t] += "0"
 
     for f in range(int(n-1),int(n/2)-1,-1):
-
         gray[f] += "1"  
 
     if n >= 3:
         generate(n/2)
 
 def finalize(start,stop, ind):
-
+    
     for f in range(int(start)-1,int(stop)-1,-1):
         gray[f] += gray[start-f-1][ind]
 
